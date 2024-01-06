1970ac52cf46722d9743b04e237eb2fab0f0b383
Omar Shalaby <omarshalaby911@knight.ucf.edu>
2024-01-05 23:42:44 -0500

    Made and tested, currently running 100%

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://Fedbca123.github.io/cleanfolio',
   title: 'JS.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Omar Shalaby',
+  role: 'Software Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+    'I am a software engineer with a passion for building things. I have experience building full-stack web applications, mobile applications, and machine learning models. I am always looking for new opportunities to learn and grow.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://linkedin.com/in/omar-shalaby/',
+    github: 'https://github.com/Fedbca123',
   },
 }
 
@@ -54,18 +54,32 @@ const skills = [
   'JavaScript',
   'TypeScript',
   'React',
+  'React Native',
   'Redux',
   'SASS',
   'Material UI',
   'Git',
   'CI/CD',
+  'MongoDB',
+  'Node.js',
+  'Express.js',
   'Jest',
   'Enzyme',
+  'C#',
+  '.NET',
+  'MySQL',
+  'Python',
+  'TensorFlow',
+  'Keras',
+  'C',
+  'C++',
+  'Java',
+  'Azure',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'shalabymomar@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
